d4697ceb6c008bc4bca13a732d60f4554b6c95ab
chisomloius <chisanloius@gmail.com>
2021-08-28 15:33:33 +0100

    completed basic web structure

---
+++ script.js
@@ -0,0 +1,36 @@
+// javascript function for toggle menu toggle button
+let settingsmenu = document.querySelector(".settings-menu");
+
+function settingsMenuToggle(){
+    settingsmenu.classList.toggle("settings-menu-height");
+}
+
+
+//javascript code for dark mode
+let darkBtn = document.getElementById("dark-btn")
+
+darkBtn.onclick = function (){
+    darkBtn.classList.toggle("dark-btn-on");
+    document.body.classList.toggle("dark-theme");
+
+    if(localStorage.getItem("theme") == "light"){
+        localStorage.setItem("theme", "dark");
+    }
+    else{
+        localStorage.setItem("theme", "light");
+    }
+}
+
+
+// javascript code to make dark mode static check when chosen it remains so.
+if (localStorage.getItem("theme") == "light"){
+    darkBtn.classList.remove("dark-btn-on");
+    document.body.classList.remove("dark-theme");
+}
+else if(localStorage.getItem("theme") == "dark"){
+    darkBtn.classList.add("dark-btn-on");
+    document.body.classList.add("dark-theme");
+}
+else{
+    localStorage.setItem("theme", "light");
+}
